2a7ec9b4feba5ff7f6b02cf87f81efae6922a853
developer <info@packpay.ir>
2019-12-28 11:19:48 +0330

    Add packpay gravity

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: درگاه پکپی گرویتی فرم
+Description: افزونه درگاه پرداخت پکپی برای فرم ساز فوق پیشرفته Gravity Forms
+Version: 1.0.0
+Author: packpay.ir
+Author URI: http://packpay.ir/
+*/
+if (!defined('ABSPATH')) exit;
+require_once('packpay.php');
